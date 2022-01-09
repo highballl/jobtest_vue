b9555f1ce11d201531bed3984ea090bfed10f32d
Ji Yoon Park <jyp0293@gmail.com>
2022-01-09 22:31:44 +0900

    구조변경: 라우터 구조 잡기

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <div id="app">
-    <Start />
-    <Prepare />
-    <Process />
-    <Complete />
-    <Result />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import Start from "./components/Start.vue";
-import Prepare from "./components/Prepare.vue";
-import Process from "./components/Process.vue";
-import Complete from "./components/Complete.vue";
-import Result from "./components/Result.vue";
-
 export default {
-  components: {
-    Start,
-    Prepare,
-    Process,
-    Complete,
-    Result,
-  },
+  components: {},
 };
 </script>
 
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { StartView } from "./components/StartView.vue";
+import { PrepareView } from "./components/PrepareView.vue";
+import { ProcessView } from "./components/ProcessView.vue";
+import { CompleteView } from "./components/CompleteView.vue";
+import { ResultView } from "./components/ResultView.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routers: [
+    {
+      path: "/",
+      compontent: StartView,
+    },
+    {
+      path: "/prepare",
+      compontent: PrepareView,
+    },
+    {
+      path: "/process",
+      compontent: ProcessView,
+    },
+    {
+      path: "/complete",
+      compontent: CompleteView,
+    },
+    {
+      path: "/result",
+      compontent: ResultView,
+    },
+  ],
+});
